445ac348dbd552889741ccbae7f835416b38955e
shuyi.yang <156838991@qq.com>
2023-10-14 17:50:06 +0800

    task: add orientation for ProblemListPage

--- react-app/src/module/problem-list/ProblemListPage.js
+++ react-app/src/module/problem-list/ProblemListPage.js
@@ -12,9 +12,15 @@ import {NavigateUtils, XmsSpinView} from "sirius-react-mobile";
 import ProblemUtils from "../util/ProblemUtils";
 import {EyeFilled, HeartFilled, StarFilled} from "@ant-design/icons";
 import ProblemFilterView from "./ProblemFilterView";
+import ConfigUtils from "../../components/config/ConfigUtils";
+import HomePageLandscape from "../home/HomePageLandscape";
+import HomePagePortrait from "../home/HomePagePortrait";
+import ProblemListPagePortrait from "./ProblemListPagePortrait";
 
 
 function ProblemListPage(props) {
+    const [screenOrientationLandscape, setScreenOrientationLandscape] = useState(() => ConfigUtils.getScreenOrientationLandscape());
+
     const navigate = useNavigate();
     const location = useLocation();
     const [currPageKey] = useState(() => NavigateUtils.buildPageKey(location));
@@ -24,76 +30,10 @@ function ProblemListPage(props) {
     }, []);
 
     let init = async () => {
-        setInitLoadingFlag(true);
-        await handleFilterChange(null);
-        setInitLoadingFlag(false);
-    }
-
-    useActivate(async () => {
-        let {fromPageKey, param} = NavigateUtils.getPageResult();
-        if (currPageKey === fromPageKey) {
-            if (param?.action === "DEL") {
-                let newListData = _.reject(listData, {id: param.problemId});
-                setListData(newListData);
-            } else if (param?.action === "SAVE") {
-                let newListData = [...listData];
-                let listItem = _.find(listData, {id: param.problemId});
-                if (listItem) {
-                    let model = await ProblemUtils.loadProblemById(listItem.id);
-                    _.assign(listItem, model);
-                    listItem.$visited = true;
-                }
-                setListData(newListData);
-            } else if (param?.action === "VIEW") {
-                let newListData = [...listData];
-                let listItem = _.find(listData, {id: param.problemId});
-                if (listItem) {
-                    listItem.$visited = true;
-                }
-                setListData(newListData);
-            }
-        }
-    });
-
-    const handleFilterChange = async (filterParam) => {
-        await ProblemUtils.filterProblemList(filterParam);
-
-        pageNo.current = 1;
-        await executeQuery(false, false);
-    }
-
-    const handleLoadMore = async () => {
-        pageNo.current++;
-        await executeQuery(true);
-    }
-
-    let pageNo = useRef(0);
-    let pageSize = useRef(20);
-
-    let [listData, setListData] = useState([]);
-    let [hasMoreFlag, setHasMoreFlag] = useState(false);
-    const [initLoadingFlag, setInitLoadingFlag] = React.useState(false);
-
-    const executeQuery = async (appendFlag) => {
-        if (appendFlag === false) {
-            setListData([]);
-        }
-
-        let result = await ProblemUtils.queryFilteredProblemByPage(pageNo.current, pageSize.current);
-
-        if (result) {
-            setListData((oldData) => {
-                return [...oldData, ...result.list];
-            });
-            setHasMoreFlag(result.hasMoreFlag);
-        }
     }
 
     const handleDeleteProblem = async (problem) => {
         await ProblemUtils.deleteProblemById(problem.id, true);
-
-        let newListData = _.reject(listData, {id: problem.id});
-        setListData(newListData);
     }
 
     const navToProblemMgmtPage = (problem) => {
@@ -125,50 +65,19 @@ function ProblemListPage(props) {
                 right={<Button color="primary" fill="solid" size="small" onClick={() => navToProblemMgmtPage(null)}>新增棋局</Button>}
             >棋局列表</NavBar>
 
-            <ProblemFilterView onChange={handleFilterChange}/>
-
-            <div className="xms-page-content with-padding-top">
-
-                <List>
-                    {listData.map((problem) => {
-                        return (
-                            <SwipeAction key={problem.orderIdx} closeOnAction={true} rightActions={[
-                                {
-                                    key: 'del',
-                                    text: '删除',
-                                    color: 'danger',
-                                    onClick: () => handleDeleteProblem(problem),
-                                },
-                                {
-                                    key: 'mgmt',
-                                    text: '管理',
-                                    color: 'primary',
-                                    onClick: () => navToProblemMgmtPage(problem),
-                                },
-                            ]}
-                            >
-                                <List.Item
-                                    prefix={problem.$levelIcon}
-                                    description={problem.$introLabel}
-                                    arrow={false} extra={
-                                    <div className="flex-row">
-                                        <HeartFilled style={{fontSize: '22px', color: problem.hardFlag ? 'red' : 'transparent'}}/>
-                                        <div>
-                                            <div>E{problem.ebbinghausTimes || 0}</div>
-                                            <EyeFilled style={{color: problem.$visited ? 'black' : 'transparent'}}/>
-                                        </div>
-                                    </div>
-                                }
-                                    onClick={() => navToProblemTestPage(problem.orderIdx)}
-                                >
-                                    <div>{problem.$introValue}</div>
-                                </List.Item>
-                            </SwipeAction>
-                        )
-                    })}
-                </List>
-                {initLoadingFlag ? <XmsSpinView/> : <InfiniteScroll loadMore={handleLoadMore} hasMore={hasMoreFlag}/>}
-            </div>
+            {screenOrientationLandscape ?
+            <ProblemListPageLandscape currPageKey={currPageKey}
+                onDeleteProblem={handleDeleteProblem}
+                onNavToProblemMgmtPage={navToProblemMgmtPage}
+                onNavToProblemTestPage={navToProblemTestPage}
+            />
+            :
+            <ProblemListPagePortrait currPageKey={currPageKey}
+                onDeleteProblem={handleDeleteProblem}
+                onNavToProblemMgmtPage={navToProblemMgmtPage}
+                onNavToProblemTestPage={navToProblemTestPage}
+            />
+            }
         </div>
     )
 }
